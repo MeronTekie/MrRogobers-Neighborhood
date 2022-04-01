2f4d2e66f34764e4b768442b196df741a434073f
Meron G.Tekie <mtekiesd210309@gmail.com>
2022-04-01 11:49:20 -0700

    DBUG: Fixing the display issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,42 @@
-let num = 95;
+//Business Logic
 let array = [];
-const newArray = beepBoop();
-
-function beepBoop() {
-  if (num === 0) {
-    return ["0"];
-  } else {
-    for (let i = 0; i <= num; i++) {
-      array.push(i);
-    }
+function beepBoop(num) {
+  for (let i = 0; i <= num; i++) {
+    array.push(i);
   }
   return array;
 }
 function beepBoopArray(newArray) {
-  for (i = 0; i < newArray.length; i++) {
+  for (i = 1; i < newArray.length; i++) {
     let numString = newArray[i];
     numString = numString.toString();
     if (numString.match(3)) {
-      newArray[i] = "Won't you be my neighbor?";
+      newArray[i] = '"Wont you be my neighbor?"';
     } else if (numString.match(2)) {
-      newArray[i] = "Boop";
+      newArray[i] = '"Boop"';
     } else if (numString.match(1)) {
-      newArray[i] = "Beep";
+      newArray[i] = '"Beep"';
     }
   }
   return newArray;
 }
+
+// User Interface Logic
+$(document).ready(function () {
+  $("form#beepBoop").submit(function (event) {
+    event.preventDefault();
+    $("#result").addClass("hidden");
+    const zero = ["0"];
+    const num = parseInt($("#number").val());
+    const newArray = beepBoop(num);
+    const finalArray = beepBoopArray(newArray);
+
+    if (num === 0) {
+      $("#output").text("[0]");
+    } else {
+      $("#result").addClass("hidden");
+      $("#result").removeClass("hidden");
+      $("#output").text(finalArray);
+    }
+  });
+});
